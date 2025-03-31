de1f29f885930a9bbdaeaff181aefe9e2c00b694
thiago0yb <thiago0yb@gmail.com>
2025-03-31 18:56:50 +0000

    criação da função para calcular todos os resultados

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 let bill = 0
-let tip = 0
+let tipPercentage = 0 
 let numberOfPeople = 0
 let buttonSelected = null
 
 function receiveBillValue() {
     bill = document.querySelector("#bill").valueAsNumber
+    calculateResults()
 }
 
-function receiveNumberOfPeople() {
+function receiveNumberOfPeopleValue() { 
     numberOfPeople = document.querySelector("#people").valueAsNumber
+    calculateResults()
 }
 
 function receiveTipPercentageValue(value) {
     tipPercentage = value / 100
 
- document.querySelector("#custom-tip").value = ""
+    document.querySelector("#custom-tip").value = ""
 
     if (buttonSelected !== null) {
         buttonSelected.classList.remove("button-selected")
@@ -22,17 +24,32 @@ function receiveTipPercentageValue(value) {
 
     buttonSelected = document.querySelector(`#button-${value}`)
     buttonSelected.classList.add("button-selected")
+
+    calculateResults()
 }
 
 function receiveCustomTipPercentageValue() {
     tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
 
     removeClassButtonSelected()
+    calculateResults()
 }
 
-    function removeClassButtonSelected() {
-        if (buttonSelected !== null) {
-            buttonSelected.classList.remove("button-selected")
-            buttonSelected = null
-      }
-  }
+function removeClassButtonSelected() {
+    if (buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null
+    }
+}
+
+function calculateResults() {
+    if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0) {
+        let strongAmount = document.querySelector(".amount strong")
+        let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
+        strongAmount.innerText = `$${tipAmountPerson.toFixed(2)}`
+
+        let strongTotal = document.querySelector(".total strong")
+        let totalPerson = (bill / numberOfPeople) + tipAmountPerson
+        strongTotal.innerText = `$${totalPerson.toFixed(2)}`
+    }
+}
